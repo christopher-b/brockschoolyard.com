dbf806602da5fac2c6833c64e66b3406ff5542fd
Christopher Bennell <cbennell@hey.com>
2026-05-29 12:19:19 -0400

    Clean up cloudflare config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig, fontProviders } from "astro/config";
-
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://brockschoolyard.com",
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare(),
 });
 
